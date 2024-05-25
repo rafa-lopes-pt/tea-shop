002b58d5837eb7ba61a84f7c1f40aa9922e7dd98
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-25 19:49:59 +0100

    style basic layout with frame and inner section

---
+++ frontend/src/pages/misc/Frame.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Frame() {
+	return (
+		<div className="frame">
+			<div className=" frame__corner frame__corner--top-left"></div>
+			<div className=" frame__corner frame__corner--bottom-right"></div>
+		</div>
+	);
+}
