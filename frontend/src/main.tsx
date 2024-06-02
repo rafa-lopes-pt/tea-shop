6bf5c4c9fe4e489d01a2d2c793364ba49d7b564a
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-02 14:19:59 +0100

    create shop context and implement it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import Router from "./pages/router.tsx";
 import "./sass/sass.css";
 import { AuthCtxProvider } from "./store/auth.context.tsx";
+import { ShopDataProvider } from "./store/shop/shop-data.context.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<AuthCtxProvider>
-			<Router />
+			<ShopDataProvider>
+				<Router />
+			</ShopDataProvider>
 		</AuthCtxProvider>
 	</React.StrictMode>
 );
---
+++ frontend/src/store/shop/shop-data.context.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { ShopItemSchemaType } from "../../../../shared/schemas/shop-item.schema";
+import CyclicArray from "../../../../shared/types/CyclicArray.class";
+import ServerAPI from "../../apis/server.endpoints";
+
+type ShopDataCtxProperties = {
+	data: CyclicArray<ShopItemSchemaType> | null;
+};
+
+export const ShopDataCtx = createContext<ShopDataCtxProperties | null>(null);
+
+/*
+
+NOTE: Write small note about implementing a reducer to manage sections
+
+*/
+
+export const ShopDataProvider = ({ children }: { children?: ReactNode }) => {
+	const [data, setData] = useState<CyclicArray<ShopItemSchemaType> | null>(
+		null
+	);
+
+	useEffect(() => {
+		ServerAPI.getShopItems().then((res: any) => {
+			setData(new CyclicArray(...res));
+		});
+	}, []);
+
+	return (
+		<ShopDataCtx.Provider value={{ data }}>{children}</ShopDataCtx.Provider>
+	);
+};
