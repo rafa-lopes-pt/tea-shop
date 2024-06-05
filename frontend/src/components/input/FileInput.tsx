87131607cbe963d58a1760c554271fa9e51eee31
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-05 21:26:34 +0100

    create file input

---
+++ frontend/src/components/input/FileInput.tsx
@@ -0,0 +1,45 @@
+import { ChangeEvent, useRef, useState } from "react";
+export default function FileInput({
+	initialValue,
+	className = "",
+	onChange,
+	alt,
+}: {
+	initialValue?: string;
+	className?: string;
+	onChange: Function;
+	alt?: string;
+}) {
+	const [src, setSrc] = useState(
+		initialValue || "media/user-image-placeholder.jpg"
+	);
+
+	const handleImageUpload = (e: ChangeEvent) => {
+		const file = (e.target as HTMLInputElement).files?.[0];
+
+		if (!file) throw new Error("Unable to get file from input");
+
+		const img = URL.createObjectURL(file);
+		setSrc(img);
+		onChange(img);
+		return;
+	};
+
+	const ref = useRef<HTMLInputElement | null>(null);
+
+	return (
+		<div
+			className={"file-input " + className}
+			onClick={() => ref.current?.click()}>
+			<img
+				src={src}
+				alt={alt}
+			/>
+			<input
+				ref={ref}
+				className="visually-hidden "
+				type="file"
+				onChange={handleImageUpload}></input>
+		</div>
+	);
+}
