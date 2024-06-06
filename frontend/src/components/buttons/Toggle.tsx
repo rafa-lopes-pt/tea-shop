a3bb9d4111f7e33d5799dbcf2dbee0776b41f312
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-06 14:18:20 +0100

    update toggle button

--- frontend/src/components/buttons/Toggle.tsx
+++ frontend/src/components/buttons/Toggle.tsx
@@ -1,21 +1,28 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, Ref, forwardRef } from "react";
 
 interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
-	// label: string;
+	label?: string;
 };
 
-export default function Toggle({ children, ...props }: ToggleProps) {
-	return (
-		<label className="toggle-wrapper">
-			{children && <span className="toggle-label">{children}</span>}
+const Toggle = forwardRef(
+	(
+		{ label, className = "", ...props }: ToggleProps,
+		ref: Ref<HTMLInputElement>
 
-			<input type="checkbox" className="toggle__input" aria-hidden  {...props} />
-			<div className="toggle">
-				<div className="toggle__slider-wrapper">
+	) => {
+		return (
+			<label className={"toggle-wrapper " + className}>
+				{label && <span className="toggle-label">{label}</span>}
 
-					<div className="toggle__slider"></div>
+				<input {...props} type="checkbox" className="toggle__input" aria-hidden ref={ref} />
+				<div className="toggle">
+					<div className="toggle__slider-wrapper">
+
+						<div className="toggle__slider"></div>
+					</div>
 				</div>
-			</div>
-		</label>
-	);
-}
+			</label>
+		);
+	})
+
+export default Toggle
