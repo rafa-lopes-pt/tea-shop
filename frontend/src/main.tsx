77897a9e113a59931a242dec8a9e5b1da2d3813a
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-07 14:32:02 +0100

    fix imports on main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactToastsContainer from "./components/alerts/toasts/ReactToastsContainer.tsx";
 import Router from "./pages/router.tsx";
 import "./sass/sass.css";
 import { AuthCtxProvider } from "./store/auth.context.tsx";
 import { ShopDataProvider } from "./store/shop/shop-data.context.tsx";
-import ReactToastsContainer from "./components/toasts/ReactToastsContainer.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
