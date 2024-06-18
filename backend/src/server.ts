7f6d6aec58e4a4ea46207ba41733d25582cc0466
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-18 20:10:36 +0100

    add error handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import HTTPCodes from "simple-http-codes";
 import router from "./routes/router";
+import HttpError from "./utils/HttpError";
 
 const server = express();
 server.use(morgan("dev"));
@@ -17,7 +18,15 @@ server.get("/health", (_, res) => {
 
 server.use(router);
 
+server.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	console.error(err);
 
+	if (err instanceof HttpError) {
+		return res.status(err.status).json(err.message);
+	}
+
+	return res.status(500).json("Unexpected Server Error");
+});
 
 server.use("*", (_, res) => {
 	res.status(HTTPCodes.ServerError.NOT_IMPLEMENTED).json(
