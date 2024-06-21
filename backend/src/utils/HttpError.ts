f1c71762bf37e80ca786020e7cc6fce2591f3eed
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-21 18:42:43 +0100

    improve HttpError class

--- backend/src/utils/HttpError.ts
+++ backend/src/utils/HttpError.ts
@@ -5,36 +5,28 @@ const ErrorCodes = {
 	...HTTPCodes.ServerError,
 } as const;
 
-type HttpErrorContext = { error: Error; cause: string };
+type HttpErrorContext = {
+	error?: Error;
+	cause?: string;
+	context?: any;
+};
 
 export default class HttpError extends Error {
-	private _statusCode: number;
-	private _context?: HttpErrorContext;
-
+	statusCode: number;
+	context?: HttpErrorContext;
 	/**
-	 *
-	 * @param httpError A valid property of the HTTPCodes object, related to an error (4xx or 5xx).
+	 * @param statusCode A valid property of the HTTPCodes object, related to an error (4xx or 5xx).
 	 * @param message Defaults to the HTTP status code name
 	 * @param context An object containing the original error and a string explaining it's cause.
 	 */
 	constructor(
-		httpError: keyof typeof ErrorCodes,
+		statusCode: keyof typeof ErrorCodes,
 		message?: string,
 		context?: HttpErrorContext
 	) {
-		super(message || httpError.split("_").join(" ").toLowerCase());
-		this._statusCode = ErrorCodes[httpError];
-		this._context = context;
-		this.name = httpError;
-	}
-
-	get status() {
-		return this._statusCode;
-	}
-	get error() {
-		return this._context?.error;
-	}
-	get cause() {
-		return this._context?.cause;
+		super(message || statusCode.split("_").join(" ").toLowerCase());
+		this.statusCode = ErrorCodes[statusCode];
+		this.context = context;
+		this.name = statusCode;
 	}
 }
