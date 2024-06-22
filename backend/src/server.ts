dcc60e9ba045dc2f4d12d75d0a13ea4f9572a658
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-23 00:25:57 +0100

    add cookie parser and timeout middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,13 +4,17 @@ import morgan from "morgan";
 import HTTPCodes from "simple-http-codes";
 import router from "./routes/router";
 import HttpError from "./utils/HttpError";
-
+import timeoutMiddleware from "./routes/middleware/timeout.middleware";
+import cookieParser from "cookie-parser";
 const server = express();
 server.use(morgan("dev"));
 server.use(cors());
+server.use(cookieParser());
 server.use(express.json());
+server.use(timeoutMiddleware());
 
 //====== ROUTES
+
 server.get("/health", (_, res) => {
 	res.status(HTTPCodes.ClientError.IM_A_TEAPOT).json({
 		message: "Server is running, but refused to brew coffee with a teapot",
@@ -21,24 +25,26 @@ server.use(router);
 
 //====== ERROR HANDLING
 
-server.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
-	if (err instanceof HttpError) {
-		err.log();
+server.use((error: Error, _req: Request, res: Response, next: NextFunction) => {
+	if (error instanceof HttpError) {
+		error.log();
 		return res
-			.status(err.statusCode)
-			.json({ ...err, message: err.message });
+			.status(error.statusCode)
+			.json({ ...error, message: error.message });
 	}
 
-	console.error(err);
+	console.error(error);
 
-	res.status(500).json(
-		"Unexpected Server Error, please contact development team"
-	);
+	return res.status(500).json({
+		message: "Unexpected Server Error, please contact development team",
+		error,
+	});
 });
+
 server.use("*", (_, res) => {
-	res.status(HTTPCodes.ServerError.NOT_IMPLEMENTED).json(
-		"Endpoint not implemented"
-	);
+	res.status(HTTPCodes.ServerError.NOT_IMPLEMENTED).json({
+		message: "Endpoint not implemented",
+	});
 });
 
 export default server;
