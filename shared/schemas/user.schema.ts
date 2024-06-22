39780235985348bac054e7b5756f1194a64dd6aa
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-23 00:23:22 +0100

    update shared schemas

--- shared/schemas/user.schema.ts
+++ shared/schemas/user.schema.ts
@@ -1,13 +1,14 @@
 import z from "zod";
 import ZodValidatorSchema from "./zod-validator-schema";
 import { BillingInfoSchema } from "./billing-info.schema";
-export const UserSchema = z.object({
-	//FIX: image probably should be validated like this...
-	image: ZodValidatorSchema.requiredNonEmptyString,
-	email: ZodValidatorSchema.email,
-	name: ZodValidatorSchema.alphabeticOnly,
-	notifyByEmail: ZodValidatorSchema.boolean,
-	notifyBySms: ZodValidatorSchema.boolean,
-	billingInfo: BillingInfoSchema,
-});
+import { SignupSchema } from "./signup.schema";
+export const UserSchema = SignupSchema.omit({ password: true }).and(
+	z.object({
+		//FIX: image probably should be validated like this...
+		image: ZodValidatorSchema.requiredNonEmptyString.optional(),
+		notifyByEmail: ZodValidatorSchema.boolean,
+		notifyBySms: ZodValidatorSchema.boolean,
+		billingInfo: BillingInfoSchema.optional(),
+	})
+);
 export type UserSchemaType = z.infer<typeof UserSchema>;
