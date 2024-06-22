58dc6a118d421f9dc6586d89079563b3c81e0628
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-23 00:29:05 +0100

    add cleanup function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
+import MongoClientWrapper from "./database/MongoClientWrapper";
 import server from "./server";
+import dbClient from "./database/DatabaseClient";
 
 dotenv.config();
 
@@ -8,3 +10,15 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
 	console.log(`[server]: server is running on port ${port}`);
 });
+
+function cleanup() {
+	try {
+		dbClient.close();
+	} catch (error) {
+		//trying to close nonexistent connections
+	}
+
+	console.log("a cleanup was performed");
+}
+process.on("SIGINT", cleanup);
+process.on("uncaughtException", cleanup);
