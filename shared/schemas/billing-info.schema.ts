d4a11879d09d50a68326e9f68869727967f678e6
Rafa Lopes <rafalopessecond@gmail.com>
2024-06-23 17:32:29 +0100

    fix schemas...once again

--- shared/schemas/billing-info.schema.ts
+++ shared/schemas/billing-info.schema.ts
@@ -1,10 +1,12 @@
 import z from "zod";
 import ZodValidatorSchema from "./zod-validator-schema";
 
-export const BillingInfoSchema = z.object({
-	country: ZodValidatorSchema.alphabeticWithWhiteSpaces,
-	city: ZodValidatorSchema.alphabeticWithWhiteSpaces,
-	street: ZodValidatorSchema.alphanumericWithWhiteSpaces,
-	zipCode: ZodValidatorSchema.zipCode,
-});
+export const BillingInfoSchema = z
+	.object({
+		country: ZodValidatorSchema.alphabeticWithWhiteSpaces,
+		city: ZodValidatorSchema.alphabeticWithWhiteSpaces,
+		street: ZodValidatorSchema.alphanumericWithWhiteSpaces,
+		zipCode: ZodValidatorSchema.zipCode,
+	})
+	.partial();
 export type BillingInfoSchemaType = z.infer<typeof BillingInfoSchema>;
--- shared/schemas/user.schema.ts
+++ shared/schemas/user.schema.ts
@@ -3,12 +3,13 @@ import ZodValidatorSchema from "./zod-validator-schema";
 import { BillingInfoSchema } from "./billing-info.schema";
 import { SignupSchema } from "./signup.schema";
 export const UserSchema = SignupSchema.omit({ password: true }).merge(
-	z.object({
-		//FIX: image probably should be validated like this...
-		image: ZodValidatorSchema.requiredNonEmptyString.optional(),
-		notifyByEmail: ZodValidatorSchema.boolean,
-		notifyBySms: ZodValidatorSchema.boolean,
-		billingInfo: BillingInfoSchema.optional(),
-	})
+	z
+		.object({
+			//FIX: image probably should be validated like this...
+			image: ZodValidatorSchema.requiredNonEmptyString.optional(),
+			notifyByEmail: ZodValidatorSchema.boolean,
+			notifyBySms: ZodValidatorSchema.boolean,
+		})
+		.merge(BillingInfoSchema)
 );
 export type UserSchemaType = z.infer<typeof UserSchema>;
