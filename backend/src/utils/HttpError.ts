8e7ceafd0f98efebbb4fa5c960e721acd117b1d6
Rafa Lopes <rafalopessecond@gmail.com>
2024-07-03 21:28:45 +0100

    improve HttpError

--- backend/src/utils/HttpError.ts
+++ backend/src/utils/HttpError.ts
@@ -5,6 +5,18 @@ const ErrorCodes = {
 	...HTTPCodes.ServerError,
 } as const;
 
+type ErrorNumbers = (typeof ErrorCodes)[keyof typeof ErrorCodes];
+type ErrorNames = keyof typeof ErrorCodes;
+
+const getErrorName = (code: ErrorNumbers) =>
+	[...Object.entries(ErrorCodes)].filter(
+		([_key, value]) => value === code
+	)[0][0];
+
+const defaultErrorMessage = (
+	code: (typeof ErrorCodes)[keyof typeof ErrorCodes]
+) => getErrorName(code).split("_").join(" ").toLowerCase();
+
 type HttpErrorContext = {
 	error?: unknown;
 	cause?: string;
@@ -12,7 +24,7 @@ type HttpErrorContext = {
 };
 
 export default class HttpError extends Error {
-	statusCode: number;
+	statusCode: ErrorNumbers;
 	context?: HttpErrorContext;
 	/**
 	 * @param statusCode A valid property of the HTTPCodes object, related to an error (4xx or 5xx).
@@ -20,14 +32,15 @@ export default class HttpError extends Error {
 	 * @param context An object containing the original error and a string explaining it's cause.
 	 */
 	constructor(
-		statusCode: keyof typeof ErrorCodes,
-		message?: string,
+		statusCode: ErrorNumbers,
+		message?: string | ErrorNames,
 		context?: HttpErrorContext
 	) {
-		super(message || statusCode.split("_").join(" ").toLowerCase());
-		this.statusCode = ErrorCodes[statusCode];
+		const errName = getErrorName(statusCode);
+		super(message || defaultErrorMessage(statusCode));
+		this.statusCode = statusCode;
 		this.context = context;
-		this.name = statusCode;
+		this.name = errName;
 	}
 
 	log() {
