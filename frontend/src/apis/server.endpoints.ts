f041462401b5673e5d3108ad50d4113c2075c1a3
Rafa Lopes <rafalopessecond@gmail.com>
2024-07-08 22:12:13 +0100

    define endpoint functions

--- frontend/src/apis/server.endpoints.ts
+++ frontend/src/apis/server.endpoints.ts
@@ -1,47 +1,57 @@
+import { UpdateProfileSchemaType } from "../../../shared/schemas/UpdateProfile.schema";
 import { LoginSchemaType } from "../../../shared/schemas/login.schema";
 import { SignupSchemaType } from "../../../shared/schemas/signup.schema";
-import { UpdateProfileSchemaType } from "../../../shared/schemas/UpdateProfile.schema";
+import HttpError from "../../../shared/types/HttpError/HttpError.type";
 
 const BASE_URI: string = import.meta.env.VITE_REST_API_URI;
-type ApiMethods = "POST" | "GET" | "PATCH" | "DELETE";
+if (!BASE_URI) {
+	throw new Error("Could not load 'REST_API_URI' from env");
+}
 
 namespace RestAPI {
+	type ApiMethods = "POST" | "GET" | "PATCH" | "DELETE";
+	export type Request = (data?: unknown) => Promise<Response>;
+	export type ResponseData<dto> = Promise<{ data: dto | string } | HttpError>;
+
 	async function baseFetch(
 		endpoint: string,
 		method: ApiMethods = "GET",
 		body: unknown = undefined
 	) {
 		return await fetch(BASE_URI + endpoint, {
-			headers: {},
+			headers: { "Content-Type": "application/json" },
 			method,
-			body: body ? JSON.stringify({ data: body }) : undefined,
+			body: body ? JSON.stringify(body) : undefined,
+			credentials: "include",
 		});
 	}
-
 	//============== auth endpoints
-	export function signup(data: SignupSchemaType) {
+	export async function signup(data: SignupSchemaType) {
 		return baseFetch("/signup", "POST", data);
 	}
-	export function activate(token: string) {
+	export async function activate(token: string) {
 		return baseFetch("/activate/" + token);
 	}
-	export function login(data: LoginSchemaType) {
+	export async function login(data: LoginSchemaType) {
 		return baseFetch("/login", "POST", data);
 	}
+	export async function logout() {
+		return baseFetch("/logout");
+	}
 	//============== shop endpoints
-	export function getShopItems() {
+	export async function getShopItems() {
 		return baseFetch("/shop");
 	}
 	//============== profile endpoints
-	export function updateProfile(data: UpdateProfileSchemaType) {
+	export async function updateProfile(data: UpdateProfileSchemaType) {
 		return baseFetch("/profile", "PATCH", data);
 	}
-	export function deleteProfile() {
+	export async function deleteProfile() {
 		return baseFetch("/profile", "DELETE");
 	}
 	//============== orders endpoints
-	export function getOrders() {
-		return baseFetch("/orders");
+	export async function getOrders() {
+		return (await baseFetch("/orders")).json();
 	}
 }
 
